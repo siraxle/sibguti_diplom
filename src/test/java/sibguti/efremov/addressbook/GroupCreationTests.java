1fec4c567ff8c541c4e8713a27552c57c9bb2fe8
axle <sir.axle@yandex.ru>
2020-01-26 21:26:16 +0300

    004_Добавлен класс GroupData и произведен рефакторинг groupCreationTest

--- src/test/java/sibguti/efremov/addressbook/GroupCreationTests.java
+++ src/test/java/sibguti/efremov/addressbook/GroupCreationTests.java
@@ -12,28 +12,52 @@ public class GroupCreationTests {
   FirefoxDriver wd;
 
   @BeforeMethod
-  public void setUp() throws Exception{
+  public void setUp() throws Exception {
     System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    login("admin", "secret");
+  }
+
+  private void login(String userName, String password) {
+    wd.findElement(By.name("user")).sendKeys(userName);
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void groupCreationTest() {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).sendKeys("test1");
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).sendKeys("test3");
-    wd.findElement(By.name("submit")).click();
+    goToGroupPage("groups");
+    initGroupCreation("new");
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation(String s) {
+    wd.findElement(By.name(s)).click();
+  }
+
+  private void goToGroupPage(String groups) {
+    wd.findElement(By.linkText(groups)).click();
   }
 
   @AfterMethod
