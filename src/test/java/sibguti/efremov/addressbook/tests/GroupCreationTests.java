303ec86de28a94d4535ea4ec0f1c7d494f636188
siraxle <sir.axle@yandex.ru>
2020-03-07 17:28:10 +0300

    040_Параметризация groupCteationTest

--- src/test/java/sibguti/efremov/addressbook/tests/GroupCreationTests.java
+++ src/test/java/sibguti/efremov/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,33 @@
 package sibguti.efremov.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sibguti.efremov.addressbook.model.GroupData;
 import sibguti.efremov.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void groupCreationTest() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test 1 '").withHeader("header 1").withFooter("footer 1") });
+    list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2") });
+    list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3") });
+
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void groupCreationTest(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
