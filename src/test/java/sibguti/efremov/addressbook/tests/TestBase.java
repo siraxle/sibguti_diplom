98fa4fef135417e3fa4ae4fcab85c59a52cb07e2
siraxle <sir.axle@yandex.ru>
2020-03-12 22:36:54 +0300

    049_Протоколирование теста создания групп в log файл

--- src/test/java/sibguti/efremov/addressbook/tests/TestBase.java
+++ src/test/java/sibguti/efremov/addressbook/tests/TestBase.java
@@ -1,14 +1,22 @@
 package sibguti.efremov.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import sibguti.efremov.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 public class TestBase {
+
+  Logger logger = (Logger) LoggerFactory.getLogger(TestBase.class);
 
   public static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", CHROME));
@@ -21,6 +29,16 @@ public class TestBase {
   @AfterSuite
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
