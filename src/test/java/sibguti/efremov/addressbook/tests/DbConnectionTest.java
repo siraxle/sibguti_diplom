be60480a8bffb982c15c2b80c30ab1f34ee57245
siraxle <sir.axle@yandex.ru>
2020-03-12 23:41:49 +0300

    050_Добавлен драйвер БД mySql и dbConnectionTest

---
+++ src/test/java/sibguti/efremov/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,37 @@
+package sibguti.efremov.addressbook.tests;
+
+import org.testng.annotations.Test;
+import sibguti.efremov.addressbook.model.GroupData;
+import sibguti.efremov.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void dbConnectionTest() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet resultSet = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+      while (resultSet.next()) {
+        groups.add(new GroupData().withId(resultSet.getInt("group_id"))
+                .withName(resultSet.getString("group_name"))
+                .withHeader(resultSet.getString("group_header"))
+                .withFooter(resultSet.getString("group_footer")));
+      }
+      resultSet.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+
+}
